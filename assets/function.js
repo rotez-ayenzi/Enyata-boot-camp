29d0e3d45a9a0f7a89f4a829500061838a04779c
oluwatobiloba rotimi-ayeni <rotimiayenioluwatobiloba@gmail.com>
2020-01-30 23:02:17 +0100

    function

---
+++ assets/function.js
@@ -0,0 +1,9 @@
+let x = prompt('put in the value of x');
+let n = prompt('put in the value of n');
+function pow(a,b){
+    return Math.pow (x,n);
+}
+
+    let answer = alert(pow());
+
+
---
+++ script.js
@@ -0,0 +1,13 @@
+/*alert('Hello! welcome to my page');
+alert('Kolade');
+alert('Kachi');*/
+let name;
+name = 'Mareeya';
+const age = 87;
+let name = 'solomon';
+let age = 90;
+console.log(name);
+console.log(age);
+/*console.log ('Kachi');
+prompt('what is your name','perdon');
+confirm('Do you have permission to view this page?')*/
